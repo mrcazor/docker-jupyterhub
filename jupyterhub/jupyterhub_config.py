21c9a2d7398b8586f1c08a63fed243aa1c03820c
mrcazor <myworkpython@mail.ru>
2022-01-09 18:06:35 +0300

    Добавлена конфигурация пользователей

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -1,12 +1,68 @@
-# Configure single user spawner
-import os
+# JupyterHub configuration
+#
+## If you update this file, do not forget to delete the `jupyterhub_data` volume before restarting the jupyterhub service:
+##
+##     docker volume rm jupyterhub_jupyterhub_data
+##
+## or, if you changed the COMPOSE_PROJECT_NAME to <name>:
+##
+##    docker volume rm <name>_jupyterhub_data
+##
 
+# Configuration file for Jupyter Hub
+
+import os
+from jupyter_client.localinterfaces import public_ips
+
+c = get_config()
+
+## Generic
+c.JupyterHub.admin_access = True
+c.Spawner.default_url = '/lab'
+
+
+## Docker spawner
 c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
 c.DockerSpawner.image = os.environ['DOCKER_JUPYTER_IMAGE']
 c.DockerSpawner.network_name = os.environ['DOCKER_NETWORK_NAME']
-c.JupyterHub.hub_ip = os.environ['HUB_IP']
+# The docker instances need access to the Hub, so the default loopback port doesn't work:
+c.JupyterHub.hub_ip = public_ips()[0]
 
-## Configure authentication (delagated to GitLab)
-from oauthenticator.gitlab import GitLabOAuthenticator
+# user data persistence
+# see https://github.com/jupyterhub/dockerspawner#data-persistence-and-dockerspawner
+notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/jovyan'
+c.DockerSpawner.notebook_dir = notebook_dir
+c.DockerSpawner.volumes = { 'jupyterhub-user-{username}': notebook_dir }
 
-c.JupyterHub.authenticator_class = GitLabOAuthenticator
+# Other stuff
+c.Spawner.cpu_limit = 2
+c.Spawner.mem_limit = '10G'
+
+# OAuth with GitHub
+c.JupyterHub.authenticator_class = 'oauthenticator.GitHubOAuthenticator'
+
+c.Authenticator.whitelist = whitelist = set()
+c.Authenticator.admin_users = admin = set()
+
+join = os.path.join
+here = os.path.dirname(__file__)
+with open(join(here, 'userlist')) as f:
+    for line in f:
+        if not line:
+            continue
+        parts = line.split()
+        name = parts[0]
+        whitelist.add(name)
+        if len(parts) > 1 and parts[1] == 'admin':
+            admin.add(name)
+
+c.GitHubOAuthenticator.oauth_callback_url = os.environ['OAUTH_CALLBACK_URL']
+
+# ssl config
+ssl = join(here, 'ssl')
+keyfile = join(ssl, 'server.key')
+certfile = join(ssl, 'server.cert')
+if os.path.exists(keyfile):
+    c.JupyterHub.ssl_key = keyfile
+if os.path.exists(certfile):
+    c.JupyterHub.ssl_cert = certfile
