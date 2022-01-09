bb7e2eba82a478c3bcde34a1f034fd427976adab
mrcazor <myworkpython@mail.ru>
2022-01-09 14:14:24 +0300

    Добавлен алгоритм аутентификации через гит лаб; исправлено наименование образа jupyter в сервисе jupyterhub

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -1,6 +1,12 @@
+# Configure single user spawner
 import os
 
 c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
 c.DockerSpawner.image = os.environ['DOCKER_JUPYTER_IMAGE']
 c.DockerSpawner.network_name = os.environ['DOCKER_NETWORK_NAME']
 c.JupyterHub.hub_ip = os.environ['HUB_IP']
+
+## Configure authentication (delagated to GitLab)
+from oauthenticator.gitlab import GitLabOAuthenticator
+
+c.JupyterHub.authenticator_class = GitLabOAuthenticator
